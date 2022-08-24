45e3ce7db165f30b33c0c373d1a94cc4a5224604
JE-UST <228612@ust-global.com>
2022-08-24 09:40:28 -0500

    Learning - AssertEquals using arrays

--- src/main/java/org/udemy/junit/implementations/utilities/Utilities.java
+++ src/main/java/org/udemy/junit/implementations/utilities/Utilities.java
@@ -4,7 +4,21 @@ public class Utilities {
 
     public String concatStrings(String a, String b)
     {
-        return a.concat(b);
+        if(a != null && b != null){
+            return a.concat(b);
+        }
+
+        return null;
+    }
+
+    public String[] getArrayStrings(String ... strings){ //This definition means that the parameters can be multiple but the same data type (more than 1 string)
+        String [] array = new String[strings.length];
+
+        for(int  i = 0; i < strings.length; i++){
+            array[i] = strings[i];
+        }
+
+        return array;
     }
 
 }
--- src/test/java/org/udemy/junit/implementation/utilities/UtilitiesTest.java
+++ src/test/java/org/udemy/junit/implementation/utilities/UtilitiesTest.java
@@ -1,21 +1,51 @@
 package org.udemy.junit.implementation.utilities;
 
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.udemy.junit.implementations.utilities.Utilities;
 
+import static org.junit.Assert.*;
+
 public class UtilitiesTest {
+    private Utilities utilities;
+
+    @Before
+    public void setUp() {
+        utilities = new Utilities();
+    }
 
     @Test
-    public void testConcatStrings()
-    {
-        //Given (Instance the class)
-        Utilities utilities = new Utilities();
-
+    public void concatStringsTest() {
         //When (Execute logic to test)
         String result = utilities.concatStrings("Hello ", "World!");
 
         //Then (Validations)
-        Assert.assertEquals("String are not equals", "Hello World!", result); //First parameter can be used as error message
+        assertEquals("String are not equals", "Hello World!", result); //First parameter can be used as error message
+    }
+
+    @Test
+    public void getArrayStringTest() {
+        //Given
+        String[] expected = {"uno", "dos", "tres"};
+
+        //When
+        String[] result = utilities.getArrayStrings("uno", "dos", "tres"); //Passing more than 1 param of string
+
+        //Then
+        //Comprobe whole array
+        assertArrayEquals(expected, result);
+        assertEquals(3, result.length);
+        assertEquals(expected.length, result.length);
+        //Compare each value with their position in the other array
+        assertEquals(expected[0], result[0]);
+        assertEquals(expected[1], result[1]);
+        assertEquals(expected[2], result[2]);
+        //Compare that values are not equals to the other
+        assertNotEquals(expected[0], result[1]);
+        assertNotEquals(expected[0], result[2]);
+        assertNotEquals(expected[1], result[0]);
+        assertNotEquals(expected[1], result[2]);
+        assertNotEquals(expected[2], result[0]);
+        assertNotEquals(expected[2], result[1]);
     }
 }
