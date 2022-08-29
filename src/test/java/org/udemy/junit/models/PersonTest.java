38a025adab0264df62a483974e8e618212a19353
JE-UST <228612@ust-global.com>
2022-08-28 22:22:47 -0500

    Learning - FixMethodOrder to order test execution

--- src/test/java/org/udemy/junit/models/PersonTest.java
+++ src/test/java/org/udemy/junit/models/PersonTest.java
@@ -2,8 +2,11 @@ package org.udemy.junit.models;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class PersonTest { //Test class need to be "public"
 
     private Person person; //Declare the object/class to test
